f0a10b6c0509d873d20e6f302e0540a7b04d194b
Santiago Betancur <santiago-betancur@hotmail.com>
2021-02-08 23:36:24 -0500

    initial commit

---
+++ src/test/java/com/sbvdeveloper/apirest/SpringBootApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbvdeveloper.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
